b585c5d5a88850ec1d05e5af0f2e33adbb81a07c
andreapanico12 <andreapanico12@gmail.com>
2024-11-20 15:08:05 +0100

    feat: add solution to bonus exercise

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -10,9 +10,23 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function saluto(nome){
+  let message
+  let date = new Date()
+  if ( date.getHours() <= 13)
+    message = `Buongiorno ${nome}`;
+  else if (date.getHours()<= 17)
+    message = `Buon pomeriggio ${nome}`;
+  else if (date.getHours > 17)
+    message = `Buonasera ${nome}`
+
+  return message
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(name) );
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
