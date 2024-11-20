f59049e40bb0a47af847c695395982b63c023d0a
andreapanico12 <andreapanico12@gmail.com>
2024-11-20 14:02:30 +0100

    feat: add solution of exercise Filtra iniziali with arrow function

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const letterfilter = ( arrayStringhe, lettera) => {
+
+  let arrayvuoto = [];
+
+  for (i=0; i<arrayStringhe.length; i++){
+
+    if (arrayStringhe[i].charAt(0) == lettera.toUpperCase()){
+
+        arrayvuoto.push(arrayStringhe[i]);
+    }
+    
+  }
+
+   return arrayvuoto; 
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(letterfilter(names, `a`));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
