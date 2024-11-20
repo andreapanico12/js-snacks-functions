a5193a5bf71f7ba988077cb9f2b17b15d13b3184
andreapanico12 <andreapanico12@gmail.com>
2024-11-20 13:36:55 +0100

    feat: solve iniziali exercise with arrow function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,8 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// ARROW FUNCTION
+
+const inizialiArrow = (array) => {
+  let arrayvuoto = []
+  for (i=0; i<array.length; i++)
+    arrayvuoto.push(array[i].charAt(0))
+
+  return arrayvuoto
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(inizialiArrow(names))
 
 
 
